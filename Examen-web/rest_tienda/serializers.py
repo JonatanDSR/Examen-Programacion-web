867b59298b5d6262a636e0d6377addad73a72419
JonatanDSR <jona.sandoval@duocuc.cl>
2022-07-13 00:24:50 -0400

    borrado

--- Examen-web/rest_tienda/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from core.models import Subscripcion
-
-class SubscripcionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscripcion
-        fields = ['usuario', 'vigente']
--- Examen-web/rest_tienda/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rest_tienda.views import obtener_sub
-
-urlpatterns = [
-    path('obtener_sub', obtener_sub, name="obtener_sub"),
-]
